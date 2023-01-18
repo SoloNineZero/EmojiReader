b210d425e7d641e7184dfec944ec299e0ab07c35
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-18 12:34:56 +0500

    Setup second screen interface

---
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NewEmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by Игорь Солодянкин on 18.01.2023.
+//
+
+import UIKit
+
+class NewEmojiTableViewController: UITableViewController {
+
+    @IBOutlet weak var emojiTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func textChange(_ sender: UITextField) {
+        
+    }
+}
